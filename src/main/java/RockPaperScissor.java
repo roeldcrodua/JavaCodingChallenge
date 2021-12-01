50f409f8ab228b832558110146ecb801fd2fac5a
Roel Crodua <roelcrodua@yahoo.com>
2021-12-01 00:56:16 -0500

    Simplify RockPaperScissor

--- src/main/java/RockPaperScissor.java
+++ src/main/java/RockPaperScissor.java
@@ -4,32 +4,16 @@ public class RockPaperScissor {
         System.out.println(rps("rock", "paper"));
     }
     public static String rps(String input1, String input2){
-        if (input1.equals("paper")){
-            if (input2.equals("scissor")){
+            if ((input1.equals("paper") && input2.equals("scissor")) ||
+                (input1.equals("scissor") && input2.equals("rock")) ||
+                (input1.equals("rock") && input2.equals("paper"))){
                 return "Player 2 wins";
-            } else if (input2.equals("rock")){
-                return "Player 1 wins";
+            } else if ((input1.equals("paper") && input2.equals("rock")) ||
+               (input1.equals("scissor") && input2.equals("paper")) ||
+               (input1.equals("rock") && input2.equals("scissor"))){
+               return "Player 1 wins";
             } else {
-                return "TIE";
+               return "TIE";
             }
-        } else if (input1.equals("scissor")){
-            if (input2.equals("rock")){
-                return "Player 2 wins";
-            } else if (input2.equals("paper")){
-                return "Player 1 wins";
-            } else {
-                return "TIE";
-            }
-        } else if (input1.equals("rock")){
-            if (input2.equals("paper")){
-                return "Player 2 wins";
-            } else if (input2.equals("scissor")){
-                return "Player 1 wins";
-            } else {
-                return "TIE";
-            }
-        } else {
-            return "Invalid input";
-        }
     }
 }
